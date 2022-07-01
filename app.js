56edcc8488cd505cf23c2605e3618747c01f0d1e
Lorreina Guyett <slorreina369@gmail.com>
2022-06-30 22:09:08 -0700

    page template set up

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import inquirer from 'inquirer';
+import fs from 'fs';
+import generatePage from './src/page-template.js';
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -133,14 +135,11 @@ promptUser()
 .then(promptProject)
 .then(portfolioData => {
     console.log(portfolioData);
+    const pageHTML = generatePage();
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if(err) throw Error(err);
+            
+        console.log('Portfolio complete! Check out index.html to see th output!');
+    });
 });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', generatePage(name, github), err =>{
-//    if(err) 'throw Error(err);
-
-//    console.log('Portfolio complete! Check out index.html to see th output!');
-// });
